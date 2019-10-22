2138b35803fdaab86dfc0e069597778b03a2a706
kaylaTill <kaylatillman319@gmail.com>
2019-10-22 14:46:55 -0700

    Search Bar Styling correctly

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -5,21 +5,8 @@ import "./search.css";
 const Search = props => {
     return (
       <div className="search-wrapper">
-        {/* <input
-          type="search"
-          onChange={e => props.onSearchVideos(e.target.value)}
-          className="search-bar"
-          placeholder="Type for search..."
-        /> */}
-
-
-
-        <SearchBar
-          type="search"
-          onChange={e => props.onSearchVideos(e.target.value)}
-          placeHolderText={"Search here..."}
-          className="search-bar"
-        />
+          <input type="search" className="search-bar"  onChange={e => props.onSearchVideos(e.target.value)} placeholder="Search.." name="search"/>
+          <button type="submit"><i class="fa fa-search"></i></button>
       </div>
     );
 }
