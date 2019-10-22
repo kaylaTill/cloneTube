4c3a420de3435d866210c0433c6b15608d152e99
kaylaTill <kaylatillman319@gmail.com>
2019-10-22 15:17:46 -0700

    Added Placeholder to Searbar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -5,7 +5,7 @@ import "./search.css";
 const Search = props => {
     return (
       <div className="search-wrapper">
-          <input type="search" className="search-bar"  onChange={e => props.onSearchVideos(e.target.value)} placeholder="Search.." name="search"/>
+          <input type="search" className="search-bar"  onChange={e => props.onSearchVideos(e.target.value)} placeholder="Search" name="search"/>
           <button className="search-button" type="submit"><i class="fa fa-search"></i></button>
       </div>
     );
