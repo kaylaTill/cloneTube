0e8b1acd0cb427ec0449f4ab252df835e8996d19
kaylaTill <kaylatillman319@gmail.com>
2019-10-22 16:55:32 -0700

    Styled all icons on right hand side

--- src/components/Nav/NavBar.jsx
+++ src/components/Nav/NavBar.jsx
@@ -3,6 +3,7 @@ import YoutubeLogo from "./icons/youtube-logo.png"
 import MenuIcon from "./icons/icons8-squared-menu-24.png"
 import NavIcon from "./NavIcon"
 import NavBell from "./icons/icons8-bell-24.png"
+import NavVideo from "./icons/icons8-video-call-24.png"
 import "./NavBar.css";
 
 
@@ -16,6 +17,7 @@ const NavBar = props => {
           <img src={YoutubeLogo} alt="Youtube Logo" className="youtube-logo" />
         </a>
         {props.children}
+        <img src={NavVideo} alt="Video-icon" className="video-icon" ></img>
         <img src={MenuIcon} alt="Menu-icon" className="menu-icon" ></img>
         <img src={NavBell} alt="Bell-icon" className="bell-icon" ></img>
       </nav>
