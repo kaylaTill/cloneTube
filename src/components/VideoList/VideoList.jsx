94f7854f84f51adfb4a62a0cb45a2eb25847d158
kaylaTill <kaylatillman319@gmail.com>
2019-10-22 17:31:11 -0700

    Transformed video list to class component

--- src/components/VideoList/VideoList.jsx
+++ src/components/VideoList/VideoList.jsx
@@ -1,14 +1,38 @@
 import React from 'react';
 import './videoList.css';
 import VideoListEntry from './VideoListEntry';
+import Switch from "react-switch";
 
-const VideoList = props => {
-  let videos = props.videos;
-  return (
-    <ul className="video-list-wrapper">
-      {videos.map(video => <VideoListEntry {...video} key={video.etag} onVideoSelect={props.onVideoSelect} />)}
-    </ul>
-  );
+
+class VideoList extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { checked: false };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(checked) {
+    this.setState({ checked });
+  }
+  
+  render() {
+    let videos = props.videos;
+    return (
+      <div>
+        <div className="next">Up next</div>
+        <div className="autoplay">
+          <label>
+            <span className="autoplay">Autoplay</span>
+            <Switch onChange={this.handleChange} checked={this.state.checked} />
+          </label>
+        </div>
+  
+        <ul className="video-list-wrapper">
+          {videos.map(video => <VideoListEntry {...video} key={video.etag} onVideoSelect={props.onVideoSelect} />)}
+        </ul>
+      </div>
+    );
+  }
 };
 
 export default VideoList;
--- src/key.js
+++ src/key.js
@@ -1,3 +1,3 @@
-const API_KEY = 'AIzaSyCih0GVSpMONXv_s6oE39f55pC9UmNnK5Y';
+const API_KEY = 'AIzaSyDLYksBowr7SVXDqUlu35U8WNED1iaNMgQ';
 
 module.exports = API_KEY;  
