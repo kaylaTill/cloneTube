ee88706f4ce91da75e4c6a78427e69bec058d2a9
kaylaTill <kaylatillman319@gmail.com>
2019-10-22 17:45:14 -0700

    Added Up Next and auto play

--- src/components/VideoList/VideoList.jsx
+++ src/components/VideoList/VideoList.jsx
@@ -6,7 +6,7 @@ import Switch from "react-switch";
 
 class VideoList extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = { checked: false };
     this.handleChange = this.handleChange.bind(this);
   }
@@ -14,21 +14,27 @@ class VideoList extends React.Component {
   handleChange(checked) {
     this.setState({ checked });
   }
-  
+
   render() {
-    let videos = props.videos;
+    let videos = this.props.videos;
     return (
       <div>
         <div className="next">Up next</div>
         <div className="autoplay">
           <label>
             <span className="autoplay">Autoplay</span>
-            <Switch onChange={this.handleChange} checked={this.state.checked} />
+            <div className="switch">
+              <Switch 
+                onChange={this.handleChange} 
+                checked={this.state.checked}
+                offColor="#787878" 
+                onColor="#787878" />
+            </div>
           </label>
         </div>
   
         <ul className="video-list-wrapper">
-          {videos.map(video => <VideoListEntry {...video} key={video.etag} onVideoSelect={props.onVideoSelect} />)}
+          {videos.map(video => <VideoListEntry {...video} key={video.etag} onVideoSelect={this.props.onVideoSelect} />)}
         </ul>
       </div>
     );
